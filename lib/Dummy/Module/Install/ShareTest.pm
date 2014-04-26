ebae48a696f5fc5e9f2596841563b7eb7f45502d
Shoichi Kaji <skaji@cpan.org>
2014-04-26 18:42:07 +0900

    init

---
+++ lib/Dummy/Module/Install/ShareTest.pm
@@ -0,0 +1,27 @@
+package Dummy::Module::Install::ShareTest;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.001";
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Dummy::Module::Install::ShareTest - hoge
+
+=head1 LICENSE
+
+Copyright (C) Shoichi Kaji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shoichi Kaji E<lt>skaji@cpan.orgE<gt>
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+
+use_ok "Dummy::Module::Install::ShareTest";
+
+done_testing;
